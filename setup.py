cb805b4f9045645a004d2233ab231fe6c5c15d6f
Tal Amuyal <talamuyal@gmail.com>
2023-03-18 23:13:38 +0100

    Build Wheels 2

--- setup.py
+++ setup.py
@@ -1,12 +1,34 @@
-# setup.py
-import os
-from distutils.core import setup
-from setuptools import setup, Extension
+import pathlib
+
+import setuptools
+
 from setuptools.command.build_ext import build_ext as _build_ext
 
-def read(filename):
-    return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
+NAME = "reverse-geocoder-whl".replace("-", "_")
+PY_SRC = NAME.replace("-", "_")
+EXT_SRC = pathlib.Path("c++") / "lib"
+
+EXT_SOURCE_FILES = [
+    str(p)
+    for ext in ("c", "cpp", "h", "hpp")
+    for p in EXT_SRC.glob(f"**/*.{ext}")
+]
+
+ext_modules = [
+    setuptools.Extension(
+        name=NAME,
+        # Sort input source files to ensure bit-for-bit reproducible builds
+        sources=sorted(EXT_SOURCE_FILES),
+        include_dirs=[],
+        library_dirs=[],
+        libraries=[],
+        language="c++",
+    ),
+]
+
+
+"""
 # Handling scipy dependency. See: http://stackoverflow.com/a/38276716
 class build_ext(_build_ext):
     def finalize_options(self):
@@ -15,18 +37,15 @@ class build_ext(_build_ext):
       __builtins__.__NUMPY_SETUP__ = False
       import numpy
       self.include_dirs.append(numpy.get_include())
+"""
 
-setup(name='reverse_geocoder_whl',
-      version='1.5.1',
-      author='Ajay Thampi',
-      author_email='ajay.thampi@gmail.com',
-      url='https://github.com/TalAmuyal/reverse-geocoder-whl',
-      packages=['reverse_geocoder_whl'],
-      package_dir={'reverse_geocoder_whl': './reverse_geocoder_whl'},
-      package_data={'reverse_geocoder_whl': ['rg_cities1000.csv']},
-      setup_requires=['numpy>=1.11.0',],
-      cmdclass={'build_ext': build_ext},
-      install_requires=['numpy>=1.11.0', 'scipy>=0.17.1',],
-      description='Fast, offline reverse geocoder',
-      license='lgpl',
-      long_description=read('README.txt'))
+
+setuptools.setup(
+    name=NAME,
+    author_email='ajay.thampi@gmail.com',
+    url='https://github.com/TalAmuyal/reverse-geocoder-whl',
+    packages=[NAME],
+    package_dir={'reverse_geocoder_whl': './reverse_geocoder_whl'},
+    package_data={'reverse_geocoder_whl': ['rg_cities1000.csv']},
+    cmdclass={'build_ext': build_ext},
+)
